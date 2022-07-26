8f85dfeff0a96038c064f5bb3d7f7a0e1895444e
Victor <victhedev@gmail.com>
2022-07-26 22:02:51 +0100

    created Banner component and applied styling

---
+++ src/components/Banner.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react'
+import axios from '../axios'
+import requests from '../requests'
+import './Banner.css'
+import { UilPlay, UilInfoCircle } from '@iconscout/react-unicons'
+
+function Banner() {
+  const [movie, setMovie] = useState([])
+  //   const imageBaseUrl = 'https://image.tmdb.org/t/p/original'
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals)
+      const randomNum = Math.floor(
+        Math.random() * request.data.results.length - 1,
+      )
+      setMovie(request.data.results[randomNum])
+    }
+    fetchData()
+  }, [])
+
+  console.log(movie)
+
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + '...' : str
+  }
+
+  return (
+    <header>
+      <div
+        className="banner"
+        style={{
+          backgroundSize: 'cover',
+          backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`,
+          backgroundPosition: 'center center',
+        }}
+      >
+        <div className="banner__contents">
+          <h1 className="banner__title">
+            {movie?.title || movie?.name || movie.origanl_name}
+          </h1>
+
+          <div classname="banner__buttons">
+            <button className="banner__button" id="play">
+              shut
+              <span>
+                <UilPlay size={13} />
+              </span>
+              Play
+            </button>
+            <button className="banner__button" id="info">
+              <span>
+                <UilInfoCircle size={13} />
+              </span>
+              More Info
+            </button>
+          </div>
+
+          <h1 className="banner__description">
+            {truncate(movie?.overview, 150)}
+          </h1>
+        </div>
+      </div>
+      <div className="banner--fadeBottom" />
+    </header>
+  )
+}
+
+export default Banner
