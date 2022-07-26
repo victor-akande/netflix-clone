480c9ad2b75c77176bbae4ad87c526ecf011a531
Victor <victhedev@gmail.com>
2022-07-26 07:35:45 +0100

    rendered row component ;  removed default react styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import './App.css';
+import './App.css'
+import Row from './components/Row'
+import requests from './requests'
 
 function App() {
   return (
     <div className="App">
-    
+      <Row
+        title="NETFLIX ORIGNALS"
+        fetchURL={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchURL={requests.fetchTrending} />
+      <Row title="Top Rated" fetchURL={requests.fetchTopRated} />
+      <Row title="Upcoming Movies" fetchURL={requests.fetchUpcoming} />
+      <Row title="Popular" fetchURL={requests.fetchPopular} />
+      <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchURL={requests.fetchComedeyMovies} />
+      <Row title="Documentaries" fetchURL={requests.fetchDocumentaries} />
+      <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} />
+      <Row title="Romance" fetchURL={requests.fetchRomanceMovies} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
