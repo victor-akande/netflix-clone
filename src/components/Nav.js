1076780f6d1471796640f3fa9a8bc4194b8a67ee
Victor Akande <victhedev@gmail.com>
2022-07-29 13:12:11 +0100

    created and styled the navbar

---
+++ src/components/Nav.js
@@ -0,0 +1,35 @@
+import { React, useEffect, useState } from "react";
+import './Nav.css'
+
+function Nav() {
+
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 100){
+        handleShow(true);
+      } else handleShow(false);
+    });
+    // return () => {
+    //   window.removeEventListener('scroll');
+    // };
+  }, []);
+
+  return (
+    <div className={`nav ${show && 'nav__black'}`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Logonetflix.png/1200px-Logonetflix.png"
+        alt="Netflix logo"
+      />
+      <img
+        className="nav__avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="Netflix avatar"
+      />
+    </div>
+  );
+}
+
+export default Nav;
