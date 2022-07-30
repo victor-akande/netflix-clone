6e35c19b5357e5e1737ea072269d6b7d94e34122
Victor Akande <victhedev@gmail.com>
2022-07-31 00:33:38 +0100

    implement the movie trailer functionality

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,11 +1,35 @@
 import React, { useEffect, useState } from 'react'
 import axios from '../axios'
 import './Row.css'
+import  YouTube from "react-youtube";
+import movieTrailer from 'movie-trailer';
 
 function Row({ title, fetchURL, isLargeRow }) {
   const [movies, setMovies] = useState([])
+  const [trailerUrl, setTrailerUrl] = useState('');
 
   const imageBaseUrl = 'https://image.tmdb.org/t/p/original'
+
+  const opts = {
+    height: '500',
+    width: '100%',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl('');
+    } else {
+      movieTrailer(movie?.name || '').then((url) => {
+        const urlVidId = new URLSearchParams(new URL(url).search);
+        setTrailerUrl(urlVidId.get('v'));
+        console.log(url, urlVidId);
+
+      }).catch(err => console.error(err))
+    }
+  }
 
   useEffect(() => {
     async function fetchData() {
@@ -29,12 +53,14 @@ function Row({ title, fetchURL, isLargeRow }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick = {() => handleClick(movie)}
             className="row__poster"
             src={`${imageBaseUrl}${movie.backdrop_path}`}
             alt={movie.name}
           />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   )
 }
